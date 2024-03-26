7f04e8b7c9a8f274d4fcaf1ff41eeb8e4741ee17
hso <sh07202@naver.com>
2024-03-26 20:05:14 +0900

    [UPDATE] placeData parent에서 전달받기

--- plrecipe/src/views/place/PlaceItemView.vue
+++ plrecipe/src/views/place/PlaceItemView.vue
@@ -1,82 +1,39 @@
 <template>
 
-
-
   <div class="places" id="places">
 
     <b-container class="bv-example-row mb-3">
-  <b-row cols="5">
+      <b-row cols="5">
+        <template v-for="place in props.placeData" :key="placeId">
+          <b-col>
+            <div class="place-block">
+              <p id="place-name">{{ place.placeName }}</p>
 
-    <template v-for="place in placeData" :key="placeId">
-      <b-col>
-      <div class="place-block">
-        <p id="place-name">{{ place.placeName }}</p>
+              <p id="star">
+                <span id="star-span" v-for='n in parseInt(place.avgStar)'>★</span>
+                <span id="star-span" v-for='n in (5 - parseInt(place.avgStar))'>☆</span>
+              </p>
 
-        <p id="star">
-          <span id="star-span" v-for='n in parseInt(place.avgStar)'>★</span>
-          <span id="star-span" v-for='n in (5 - parseInt(place.avgStar))'>☆</span>
-        </p>
+              <p id="address">{{ place.placeLocation }}</p>
 
-        <p id="address">{{ place.placeLocation }}</p>
-
-        <div id="place-category">
-          {{ place.placeCategory.placeCategoryName }}
-        </div>
-      </div>
-    </b-col>
-    </template>
-
-  </b-row>
-</b-container>
-
+              <div id="place-category">
+                {{ place.placeCategory.placeCategoryName }}
+              </div>
+            </div>
+          </b-col>
+        </template>
+      </b-row>
+    </b-container>
+    
   </div>
-
-
+  
 </template>
 
 <script setup>
-const places =
-  [
-    {
-      "placeId": 1,
-      "placeName": "보라매공원",
-      "placeLocation": "서울광역시 보라매동",
-      "placePhoneNum": null,
-      "placeCategory": {
-        "placeCategoryId": 5,
-        "placeCategoryName": "산책"
-      },
-      "avgStar": 5.0
-    },
-    {
-      "placeId": 2,
-      "placeName": "귤밭체험농장",
-      "placeLocation": "제주특별자치도 귤동",
-      "placePhoneNum": null,
-      "placeCategory": {
-        "placeCategoryId": 5,
-        "placeCategoryName": "산책"
-      },
-      "avgStar": 0.0
-    },
-    {
-      "placeId": 3,
-      "placeName": "배김윤조한공원",
-      "placeLocation": "서울광역시 신대방동",
-      "placePhoneNum": null,
-      "placeCategory": {
-        "placeCategoryId": 5,
-        "placeCategoryName": "산책"
-      },
-      "avgStar": 4.5
-    }];
-
-
-import { provide, ref, reactive } from "vue";
-
-const placeData = reactive(places);
-console.log(placeData);
-
+import { defineProps } from 'vue';
+const props = defineProps({
+  placeData: String
+});
 
 
 </script>
--- plrecipe/src/views/place/PlaceMainView.vue
+++ plrecipe/src/views/place/PlaceMainView.vue
@@ -1,13 +1,57 @@
 <script setup>
 import PlaceCategory from './PlaceCategory.vue'
 import PlaceItemView from './PlaceItemView.vue'
+
+const places =
+  [
+    {
+      "placeId": 1,
+      "placeName": "보라매공원",
+      "placeLocation": "서울광역시 보라매동",
+      "placePhoneNum": null,
+      "placeCategory": {
+        "placeCategoryId": 5,
+        "placeCategoryName": "산책"
+      },
+      "avgStar": 5.0
+    },
+    {
+      "placeId": 2,
+      "placeName": "귤밭체험농장",
+      "placeLocation": "제주특별자치도 귤동",
+      "placePhoneNum": null,
+      "placeCategory": {
+        "placeCategoryId": 5,
+        "placeCategoryName": "산책"
+      },
+      "avgStar": 0.0
+    },
+    {
+      "placeId": 3,
+      "placeName": "배김윤조한공원",
+      "placeLocation": "서울광역시 신대방동",
+      "placePhoneNum": null,
+      "placeCategory": {
+        "placeCategoryId": 5,
+        "placeCategoryName": "산책"
+      },
+      "avgStar": 4.5
+    }];
+
+
+import { provide, ref, reactive } from "vue";
+
+const placeData = reactive(places);
+const categoryCode = ref(0);
+
+
 </script>
 
 <template>
   <main>
     <PlaceCategory />
 
-    <PlaceItemView />
+    <PlaceItemView :placeData="placeData"/>
 
   </main>
 
@@ -16,5 +60,4 @@ import PlaceItemView from './PlaceItemView.vue'
 
 
 <style scoped>
-div {}
 </style>
