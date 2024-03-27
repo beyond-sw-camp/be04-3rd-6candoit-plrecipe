bc5e9475d398b31abc93115e603661f3c1fd6a6a
hso <sh07202@naver.com>
2024-03-27 17:07:57 +0900

    [UPDATE] category active class 적용

--- plrecipe/src/views/place/PlaceCategory.vue
+++ plrecipe/src/views/place/PlaceCategory.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="category">
-    <div id="All" @click="emit('change', '')">전체</div>
-    <div id="food" @click="emit('change', 1)">음식점</div>
-    <div id="cafe" @click="emit('change', 2)">카페</div>
-    <div id="culture" @click="emit('change', 3)">문화</div>
-    <div id="activity" @click="emit('change', 4)">액티비티</div>
-    <div id="walk" @click="emit('change', 5)">산책</div>
-    <div id="etc" @click="emit('change', 6)">기타</div>
+    <div id="All" @click="changeCategory('')" :class="{All: activeCategory == ''}" >전체</div>
+    <div id="food" @click="changeCategory(1)" :class="{food: activeCategory == 1}">음식점</div>
+    <div id="cafe" @click="changeCategory(2)" :class="{cafe: activeCategory == 2}">카페</div>
+    <div id="culture" @click="changeCategory(3)" :class="{culture: activeCategory == 3}">문화</div>
+    <div id="activity" @click="changeCategory(4)" :class="{activity: activeCategory == 4}">액티비티</div>
+    <div id="walk" @click="changeCategory(5)" :class="{walk: activeCategory == 5}">산책</div>
+    <div id="etc" @click="changeCategory(6)" :class="{etc: activeCategory == 6}">기타</div>
   </div>
 </template>
 
 <script setup>
-import { defineEmits } from 'vue';
+import { defineEmits, ref } from 'vue';
 
 /* 부모로부터 물려받은 toggle이라는 이름의 이벤트를 호출할 수 있는 함수 반환받기(defineEmits를 활용하며 배열로 이벤트 이름 작성) */
 const emit = defineEmits(['change']);
+const activeCategory = ref('');
+
+function changeCategory(category) {
+  console.log(category);
+  activeCategory.value = category;
+  emit('change', category);
+}
+
 </script>
 
 <style scoped>
@@ -22,7 +30,6 @@ const emit = defineEmits(['change']);
   width: 100px;
   height: 30px;
   text-align: center;
-  background-color: #ffffff;
   border: #646464 solid 1px;
   border-radius: 10px 10px 0px 0px;
   float: left;
@@ -33,28 +40,32 @@ const emit = defineEmits(['change']);
   margin-top: 20px;
   transition: all 0.3s;
 }
+#All:hover, .All {
+  background-color: khaki;
+}
 
-#food:hover {
+#food:hover, .food {
   background-color: lightpink;
 }
 
-#cafe:hover {
+#cafe:hover, .cafe {
   background-color: lightyellow;
 }
 
-#culture:hover {
+#culture:hover, .culture {
   background-color: lightgreen;
 }
 
-#activity:hover {
+#activity:hover, .activity {
   background-color: lightblue;
 }
 
-#walk:hover {
+#walk:hover, .walk {
   background-color: rgba(212, 170, 255, 0.952);
 }
 
-#etc:hover {
+#etc:hover, .etc {
   background-color: lightgray;
 }
+
 </style>
