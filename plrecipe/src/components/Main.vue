7168996f9963769789bf39f3ea871cdc2492ff0b
조수빈 <angelajsb@naver.com>
2024-03-29 10:49:38 +0900

    Main.vue 및 Main.CSS 수정 -> div 분리 및 css 주석 삭제

--- plrecipe/src/components/Main.vue
+++ plrecipe/src/components/Main.vue
@@ -1,27 +1,34 @@
 <template>
     <div class="container">
-        <div class="big-image-container">
-                <img src="@/assets/img/main.png" alt="메인 이미지" class="big-image">
-                <div class="image-text">
-                    <h1>Plrecipe</h1>
-                    <br><br>
-                    <p>플레시피(Play + Recipe)는 사용자에게 다양한 놀거리를 제공하는 서비스 입니다.<br>
-                        나만의 코스들을 조합해 다른 회원들과 공유하고 추억을 쌓아보세요!
-                    </p>
-                </div>
+    <div class="big-image-container">
+        <img src="@/assets/img/main.png" alt="메인 이미지" class="big-image">
+        <div class="image-text">
+        <h1>Plrecipe</h1>
+        <br><br>
+        <p>
+            플레시피(Play + Recipe)는 사용자에게 다양한 놀거리를 제공하는 서비스 입니다.<br>
+            나만의 코스들을 조합해 다른 회원들과 공유하고 추억을 쌓아보세요!
+        </p>
+        </div>
+    </div>
+
+    <div class="small-images">
+        <div class="image-container small-image1" @click="goPost">
+        <img src="@/assets/img/post.png" alt="왼쪽 이미지">
+        <div class="overlay-text">
+            <div class="overlay-title">게시판</div>
+            <div class="overlay-description">다양한 코스를 구성해보세요.</div>
+        </div>
         </div>
 
-        <div class="small-images">
-          <div class="image-container small-image1" @click="goPost">
-                    <img src="@/assets/img/post.png" alt="왼쪽 이미지">
-                    <div class="overlay-text">게시판<br>다양한 코스를 구성해보세요.</div>
-            </div>
-
-            <div class="image-container small-image2" @click="goPlace">
-                    <img src="@/assets/img/place.png" alt="오른쪽 이미지">
-                    <div class="overlay-text">장소<br>다양한 장소를 구경해보세요.</div>
-            </div>
+        <div class="image-container small-image2" @click="goPlace">
+        <img src="@/assets/img/place.png" alt="오른쪽 이미지">
+        <div class="overlay-text">
+            <div class="overlay-title">장소</div>
+            <div class="overlay-description">다양한 장소를 구경해보세요.</div>
         </div>
+        </div>
+    </div>
     </div>
 </template>
 
@@ -30,7 +37,7 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
-  const goPost = () => {
+const goPost = () => {
     router.push('/post');
 };
 const goPlace = () => {
@@ -39,5 +46,5 @@ const goPlace = () => {
 </script>
 
 <style scoped>
-    @import url('@/assets/css/main/Main.css');
+@import url('@/assets/css/main/Main.css');
 </style>
